aa43a795226adefbeff9ab672d53c0269570ae63
muzaffaar <muzaffart949@gmail.com>
2024-07-09 00:01:44 +0200

    deleted all extra things

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,18 +2,19 @@
 
 namespace Tests\Feature\Unit;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest
+class ExampleTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
     public function test_example(): void
     {
-        // $response = $this->get('/');
+        $response = $this->get('/');
 
-        // $response->assertStatus(200);
-        
+        $response->assertStatus(200);
     }
 }
